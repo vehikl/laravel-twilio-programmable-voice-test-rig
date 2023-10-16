7b4a691322cb7b179eecd80f4670b2b531b1af9a
Alex Barry <alex.barry@gmail.com>
2023-10-16 13:48:44 -0400

    New controller to test gather and dial

--- workbench/app/Http/Controllers/AnonymousDialing.php
+++ workbench/app/Http/Controllers/AnonymousDialing.php
@@ -3,6 +3,7 @@
 namespace Workbench\App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Twilio\TwiML\VoiceResponse;
 
 class AnonymousDialing extends Controller
@@ -16,18 +17,53 @@ class AnonymousDialing extends Controller
 
     public function gather(): VoiceResponse
     {
+        $this->voiceResponse->say('Dial North-American Number, then press pound');
+        $this->voiceResponse->gather([
+            'action' => route('anonymous-dialing.dial'),
+            'input' => 'dtmf',
+            'finishOnKey' => '#',
+            'numDigits' => 12,
+            'actionOnEmptyResult' => false,
+
+        ]);
+        $this->voiceResponse->redirect(route('anonymous-dialing.failed'), ['method' => 'POST']);
         return $this->voiceResponse;
     }
 
-    public function dial(): VoiceResponse
+    public function dial(Request $request): VoiceResponse
     {
+        $digits = preg_replace('/\D/g', '', $request->input('Digits'));
+        if (strlen($digits) === 0) {
+            $this->voiceResponse->redirect(route('anonymous-dialing.failed'), ['method' => 'POST']);
+            return $this->voiceResponse;
+        }
+
+        $digitsSplit = implode(' ', explode('', $digits));
+
+        $this->voiceResponse->say("Dialing $digitsSplit, please wait");
+        $this->voiceResponse->pause(['length' => 1]);
+
+        $this->voiceResponse->dial($digits, [
+            'action' => route('anonymous-dialing.completed'),
+            'method' => 'POST',
+        ]);
+
         return $this->voiceResponse;
     }
 
     public function completed(): VoiceResponse
     {
+        $this->voiceResponse->say('Phone call completed');
+        $this->voiceResponse->hangup();
+
         return $this->voiceResponse;
     }
 
+    public function failed(): VoiceResponse
+    {
+        $this->voiceResponse->say('Please try again later');
+        $this->voiceResponse->hangup();
+        return $this->voiceResponse;
+    }
 
 }
