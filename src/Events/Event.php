3f2a7061a5bb2a757572019b9144ce702bda350c
Alex Barry <alex.barry@gmail.com>
2023-11-21 16:37:58 -0500

    Trim down base event

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -7,23 +7,7 @@ use Vehikl\LaravelTwilioProgrammableVoiceTestRig\ProgrammableVoiceRig;
 
 class Event
 {
-    protected array $attributes = [];
-
     public function __construct(protected ProgrammableVoiceRig $rig, protected Element $element)
     {
-    }
-
-    /**
-     * @param array<string,mixed> $attributes
-     */
-    protected function with(array $attributes): ProgrammableVoiceRig
-    {
-        foreach ($attributes as $key => $value) {
-            $this->attributes[$key] = is_bool($value)
-                ? ($value ? 'true' : 'false')
-                : $value;
-        }
-
-        return $this->rig;
     }
 }
